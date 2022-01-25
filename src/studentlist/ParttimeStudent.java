86237d08ca1d567cfc32ede4ae72861d95675787
Suresh Sharma <shasures@sheridancollege.ca>
2022-01-25 12:43:18 -0500

    added parttime student

---
+++ src/studentlist/ParttimeStudent.java
@@ -0,0 +1,24 @@
+package studentlist;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Suresh Sharma
+ */
+public class ParttimeStudent extends Student{
+    private int numOfCourses;
+
+    /**
+     * @return the numOfCourses
+     */
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    /**
+     * @param numOfCourses the numOfCourses to set
+     */
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+}
